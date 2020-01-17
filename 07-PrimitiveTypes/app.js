52f8101e3124337367728458d23e66156bd50571
Lucas Werner <lwerner27@gmail.com>
2020-01-17 12:47:33 -0600

    Added the seventh section of notes and code examples.

---
+++ 07-PrimitiveTypes/app.js
@@ -0,0 +1,24 @@
+// There are six primitive types in JavaScript
+// Primitive type: type of data that represents single value (not an object).
+
+// We talked about this earlier you should never use this manually.
+var undefinedVariable = undefined;
+
+// Represents lack of existance.
+var nullVariable = null;
+
+// A true or false value, can also be thought of as a 0 (false) or a 1 (true).
+var booleanVariable = true || false;
+
+// This can be any integer or decimal number
+// Numbers in JavaScript are considered "floating point"
+// This means they always have a decimal associated with them.
+// This can make calculation weird in JavaScript
+var numberVariable = 1;
+
+// Sequence of characters in both single and double quotes
+// In other programming languages this is not considered a primitive type.
+var stringVariable = "Lucas Werner";
+
+// This is new to ES6 and we won't worry about it for now.
+var symbolVariable = Symbol();
