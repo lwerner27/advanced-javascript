e7858658c6d56ddd30da2e2e1e1b1a8d1223526b
Lucas Werner <lwerner27@gmail.com>
2020-01-19 12:42:47 -0600

    Added the ninth section of notes and code examples.

---
+++ 09-Coercion/app.js
@@ -0,0 +1,8 @@
+// Coercion is the act of converting a value from one type to another.
+// This is not uncommon in JavaScript as it is a dynamicaly typed language.
+
+// This in example of coercion
+// The number 18 will be coerced into a string.
+// Then the two strings will be concatenated.
+var todaysDate = "January " + 18;
+console.log(todaysDate);
